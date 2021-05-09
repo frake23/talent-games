d4aba08cde2da5f0c4ca13da3fac87fc4586f817
frake23 <dimsacom@gmail.com>
2021-05-10 02:13:22 +0300

    removed cringe

--- components/landing/OutroSection.tsx
+++ components/landing/OutroSection.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import TWContainer from "../tailwind/TWContainer";
 import TWRowGrid from "../tailwind/TWRowGrid";
 import TWButton from "../tailwind/TWButton";
