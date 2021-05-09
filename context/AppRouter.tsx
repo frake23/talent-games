e50636ef115ddce868cddf4ff717ff9a631a0ee1
frake23 <dimsacom@gmail.com>
2021-05-09 21:21:25 +0300

    app router context completed

--- context/AppRouter.tsx
+++ context/AppRouter.tsx
@@ -1,6 +1,7 @@
-import React, {createContext, useContext} from 'react';
+import React, {createContext, useCallback, useContext, useState} from 'react';
 import GameType from "../types/games";
 import AccountType from "../types/accounts";
+import App from "next/app";
 
 type AppRoute =
     | {
@@ -12,12 +13,28 @@ type AppRoute =
         subcategory: GameType
       }
 
-const AppRouterContext = createContext(null);
+interface AppRouterValue {
+    route: AppRoute,
+    setRoute: (route: AppRoute) => void
+}
 
-const useAppRouter = useContext(AppRouterContext);
+const AppRouterContext = createContext<AppRouterValue>(null);
+
+const useAppRouter = () => useContext<AppRouterValue>(AppRouterContext);
 
 const AppRouterContextProvider = ({ children }) => {
-
+    const [route, setRoute] = useState<AppRoute>({
+        category: "accounts",
+        subcategory: AccountType.Kruzhok
+    });
+    const onRouteChange = useCallback((route: AppRoute) => {
+        setRoute(route);
+    }, []);
+    return (
+        <AppRouterContext.Provider value={{route, setRoute: onRouteChange}}>
+            {children}
+        </AppRouterContext.Provider>
+    )
 }
 
 export {AppRouterContextProvider, useAppRouter}
--- pages/app.tsx
+++ pages/app.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import {AppRouterContextProvider} from "../context/AppRouter";
+import {Header} from "../components/Header";
 
 const AppPage = () => {
     return (
-        <>
-        </>
+        <AppRouterContextProvider>
+            <Header/>
+        </AppRouterContextProvider>
     )
 }
 
