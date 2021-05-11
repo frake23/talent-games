16029f06d3b181406714ead69d0cd06fa626818e
ekinohito <zhenyagur9@gmail.com>
2021-05-11 23:41:20 +0300

    paddings

--- components/app/сontents/UnauthorizedContent.tsx
+++ components/app/сontents/UnauthorizedContent.tsx
@@ -3,7 +3,7 @@ import TWButton from "../../tailwind/TWButton";
 
 const UnauthorizedContent = ({account}: {account: string}) => {
     return (
-        <div className={`flex flex-col items-center justify-center`}>
+        <div className={`flex flex-col items-center justify-center py-12`}>
             <span className="text-body1 mb-4">ПРИВЯЖИТЕ СВОЙ АККАУНТ {account}</span>
             <TWButton color={"emerald"} size={"default"}>Авторизоваться</TWButton>
         </div>
